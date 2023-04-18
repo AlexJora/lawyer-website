59b944b919556b6020f30464315190874001a4e7
AlexJora <jora.catalinaa@gmail.com>
2023-04-18 07:17:48 +0200

    starting services

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import { EnvelopeFill } from 'react-bootstrap-icons';
-import { TelephoneFill } from 'react-bootstrap-icons';
 
 const Home = () => {
   return (
@@ -9,8 +7,10 @@ const Home = () => {
       <section>
         {/*navbar*/}
         <Navbar />
+        {/* banner */}
+        <div style={{ backgroundColor: 'gray', width: '100', height: '4rem', color: 'white' }}> AVOCAT ALINA MARIN - BUCURESTI </div>
         {/*intro*/}
-        <section id="intro">
+        <section id="intro" class="bg-light mt-5">
           <div class="container">
             <div class="row mt-4 gx-5 flex-column flex-md-row">
               <div class="col col-md-6 order-md-2">
@@ -19,11 +19,8 @@ const Home = () => {
               <div class="col col-md-6 order-md-1 mt-5 mt-md-0">
                 <h4 class='mb-4'>Te confrunți cu o problemă juridică?</h4>
                 <p>CABINET DE AVOCAT ALINA MARIN îți stă la dispoziție. Imi asum misiunea de a oferi clienților mei cele mai bune sfaturi juridice și de a lupta pentru drepturile lor legale. Am cunoștințe solide de drept, o vastă experiență și abilitățile necesare pentru a trata cu seriozitate și profesionalism toate cazurile mele. Misiunea mea este să obțin rezultate pozitive pentru clienții mei și să-i ajut să-și atingă obiectivele juridice.</p>
-                <div class="btn-group">
-                  <a href="#" class="btn btn-warning"><EnvelopeFill />Intreaba avocatul</a>
-                  <a href="#" class="btn btn-secondary"><TelephoneFill />072336675</a>
-
-
+                <div class="btn">
+                  <button type="button" class="btn btn-outline-secondary">SOLICITA CONSULTANTA</button>
                 </div>
               </div>
             </div>
@@ -33,6 +30,109 @@ const Home = () => {
 
         {/* more-info */}
         {/* services */}
+        <section id="services" class="bg-light mt-5">
+          <div class="container-lg">
+            <div class="text-center">
+              <h5>Domenii de activitate</h5>
+              <h6 class="text-muted">Vă asigurăm servicii juridice în diverse domenii ale dreptului la standarde profesionale.</h6>
+            </div>
+            <div class="row my-5 g-3 align-items-center justify-content-center">
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                  <div class="card-body text-center py-4">
+                    <h6 class="card-title">DREPT CIVIL</h6>
+                    <GoLaw />
+                    <p class="card-text mx-1 text-muted">Servicii de consultanță juridică, asistare și reprezentare în
+                      fața instanțelor de judecată, redactare cereri de chemare în judecată. Vă ajutăm să rezolvați
+                      rapid problema juridică.</p>
+                    <a href="#">
+                      Citeste mai mult...
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                  <div class="card-body text-center py-4">
+                    <h6 class="card-title">DREPT PENAL</h6>
+                    <p class="card-text mx-1 text-muted">Asistență și reprezentare juridică în fața organelor de urmărire penală și în fața instanțelor de judecată, redactare cereri și plângeri împotriva măsurilor și actelor de urmărire penală, redactare cerere de reabilitare.</p>
+                    <a href="#">
+                      Citeste mai mult...
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                  <div class="card-body text-center py-4">
+                    <h6 class="card-title">EXECUTARE SILITA</h6>
+                    <p class="card-text mx-1 text-muted">În materia executării silite, serviciile oferite sunt indiferent de calitatea pe care o aveți în această procedură – debitor sau creditor. Pasivitatea poate avea consecințe deosebit de grave în această materie, atât pentru creditor, cât și pentru debitor.</p>
+                    <a href="#">
+                      Citeste mai mult...
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                  <div class="card-body text-center py-4">
+                    <h6 class="card-title">RECUPERARE CREANTE</h6>
+                    <p class="card-text mx-1 text-muted">Asistență și reprezentare juridică atât în cadrul demersurilor judiciare privind recuperarea creanțelor – acțiuni civile în procedura de drept comun, cereri cu valoare redusă sau ordonanțe de plată, fie pe cale amiabilă.</p>
+                    <a href="#">
+                      Citeste mai mult...
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                  <div class="card-body text-center py-4">
+                    <h6 class="card-title">DREPTUL FAMILIEI - DIVORT SI PARTAJ</h6>
+                    <p class="card-text mx-1 text-muted">Servicii de asistare și reprezentare juridică în procesele de divorț, partaj, exercitarea autorității părintești, pensie de întreținere, program de vizită minor.</p>
+                    <a href="#">
+                      Citeste mai mult...
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                  <div class="card-body text-center py-4">
+                    <h6 class="card-title">DREPTUL MUNCII</h6>
+                    <p class="card-text mx-1 text-muted">Oferim servicii de avocatură profesionale, concretizate prin redactarea contestației împotriva deciziei de concediere, împotriva deciziei de sancționare disciplinară, obligarea angajatorului la plata drepturilor de natură salarială, elaborarea de opinii juridice în dreptul muncii.</p>
+                    <a href="#">
+                      Citeste mai mult...
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                  <div class="card-body text-center py-4">
+                    <h6 class="card-title">DREPTUL COMERCIAL SI SOCIETAR</h6>
+                    <p class="card-text mx-1 text-muted">Înființări societăți comerciale la Registrul Comerțului, schimbare sediu social, înființare/radiere punct de lucru, redactare acte constitutive, hotărâri ale adunării generale a asociațiilor.</p>
+                    <a href="#">
+                      Citeste mai mult...
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card">
+                  <div class="card-body text-center py-4">
+                    <h6 class="card-title">PENSII SI ALTE DREPTURI DE ASIGURARI SOCIALE</h6>
+                    <p class="card-text mx-1 text-muted">Consultanță juridică, analizarea documentelor, redactarea contestației la decizia de pensionare, asistență și reprezentare în fața instanțelor de judecată.</p>
+                    <a href="#">
+                      Citeste mai mult...
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </section>
+
         {/* footer */}
       </section >
     </>
