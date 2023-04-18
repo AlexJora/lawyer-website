aeff71d5b7e9200bafa9502d0d32b23570d6719c
AlexJora <jora.catalinaa@gmail.com>
2023-04-18 08:27:03 +0200

    adding icons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGavel, faScaleBalanced, faTruck, faGem, faPeopleLine, faBriefcase, faScroll, faMoneyBill1Wave } from '@fortawesome/free-solid-svg-icons';
 const Home = () => {
   return (
     <>
@@ -40,8 +41,9 @@ const Home = () => {
               <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="card">
                   <div class="card-body text-center py-4">
+                    <FontAwesomeIcon icon={faScaleBalanced} />
                     <h6 class="card-title">DREPT CIVIL</h6>
-                    <GoLaw />
+
                     <p class="card-text mx-1 text-muted">Servicii de consultanță juridică, asistare și reprezentare în
                       fața instanțelor de judecată, redactare cereri de chemare în judecată. Vă ajutăm să rezolvați
                       rapid problema juridică.</p>
@@ -54,6 +56,10 @@ const Home = () => {
               <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="card">
                   <div class="card-body text-center py-4">
+
+                    <FontAwesomeIcon icon={faGavel} />
+
+
                     <h6 class="card-title">DREPT PENAL</h6>
                     <p class="card-text mx-1 text-muted">Asistență și reprezentare juridică în fața organelor de urmărire penală și în fața instanțelor de judecată, redactare cereri și plângeri împotriva măsurilor și actelor de urmărire penală, redactare cerere de reabilitare.</p>
                     <a href="#">
@@ -65,6 +71,7 @@ const Home = () => {
               <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="card">
                   <div class="card-body text-center py-4">
+                    <FontAwesomeIcon icon={faTruck} />
                     <h6 class="card-title">EXECUTARE SILITA</h6>
                     <p class="card-text mx-1 text-muted">În materia executării silite, serviciile oferite sunt indiferent de calitatea pe care o aveți în această procedură – debitor sau creditor. Pasivitatea poate avea consecințe deosebit de grave în această materie, atât pentru creditor, cât și pentru debitor.</p>
                     <a href="#">
@@ -76,6 +83,7 @@ const Home = () => {
               <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="card">
                   <div class="card-body text-center py-4">
+                    <FontAwesomeIcon icon={faGem} />
                     <h6 class="card-title">RECUPERARE CREANTE</h6>
                     <p class="card-text mx-1 text-muted">Asistență și reprezentare juridică atât în cadrul demersurilor judiciare privind recuperarea creanțelor – acțiuni civile în procedura de drept comun, cereri cu valoare redusă sau ordonanțe de plată, fie pe cale amiabilă.</p>
                     <a href="#">
@@ -87,6 +95,7 @@ const Home = () => {
               <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="card">
                   <div class="card-body text-center py-4">
+                    <FontAwesomeIcon icon={faPeopleLine} />
                     <h6 class="card-title">DREPTUL FAMILIEI - DIVORT SI PARTAJ</h6>
                     <p class="card-text mx-1 text-muted">Servicii de asistare și reprezentare juridică în procesele de divorț, partaj, exercitarea autorității părintești, pensie de întreținere, program de vizită minor.</p>
                     <a href="#">
@@ -98,6 +107,7 @@ const Home = () => {
               <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="card">
                   <div class="card-body text-center py-4">
+                    <FontAwesomeIcon icon={faBriefcase} />
                     <h6 class="card-title">DREPTUL MUNCII</h6>
                     <p class="card-text mx-1 text-muted">Oferim servicii de avocatură profesionale, concretizate prin redactarea contestației împotriva deciziei de concediere, împotriva deciziei de sancționare disciplinară, obligarea angajatorului la plata drepturilor de natură salarială, elaborarea de opinii juridice în dreptul muncii.</p>
                     <a href="#">
@@ -109,6 +119,7 @@ const Home = () => {
               <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="card">
                   <div class="card-body text-center py-4">
+                    <FontAwesomeIcon icon={faScroll} />
                     <h6 class="card-title">DREPTUL COMERCIAL SI SOCIETAR</h6>
                     <p class="card-text mx-1 text-muted">Înființări societăți comerciale la Registrul Comerțului, schimbare sediu social, înființare/radiere punct de lucru, redactare acte constitutive, hotărâri ale adunării generale a asociațiilor.</p>
                     <a href="#">
@@ -120,6 +131,7 @@ const Home = () => {
               <div class="col-lg-3 col-md-6 col-sm-12">
                 <div class="card">
                   <div class="card-body text-center py-4">
+                    <FontAwesomeIcon icon={faMoneyBill1Wave} />
                     <h6 class="card-title">PENSII SI ALTE DREPTURI DE ASIGURARI SOCIALE</h6>
                     <p class="card-text mx-1 text-muted">Consultanță juridică, analizarea documentelor, redactarea contestației la decizia de pensionare, asistență și reprezentare în fața instanțelor de judecată.</p>
                     <a href="#">
@@ -129,7 +141,7 @@ const Home = () => {
                 </div>
               </div>
             </div>
-
+            <p>Alte domenii de activitate în care vă oferim consultanță, asistare și reprezentare în fața instanțelor de judecată sau a altor instituții ale statului: RECUPERĂRI CREANȚE, ASOCIAȚII DE PROPRIETARI, PLÂNGERI CONTRAVENȚIONALE AMENZI, DESPĂGUBIRI ACCIDENTE AUTO.</p>
           </div>
         </section>
 
