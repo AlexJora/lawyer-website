351bfd3d1d7986bda0600ca29f1b8528f388b179
AlexJora <jora.catalinaa@gmail.com>
2023-04-20 15:07:46 +0200

    Acasa.js

--- client/src/pages/Acasa.js
+++ client/src/pages/Acasa.js
@@ -21,8 +21,10 @@ const Acasa = () => {
               <h4>la un proces echitabil</h4>
             </div>
           </div>
-
-
+        </section>
+        <section>
+          <h4 className='mb-4'>Te confrunți cu o problemă juridică?</h4>
+          <p>CABINET DE AVOCAT ALINA MARIN îți stă la dispoziție. Imi asum misiunea de a oferi clienților mei cele mai bune sfaturi juridice și de a lupta pentru drepturile lor legale. Am cunoștințe solide de drept, o vastă experiență și abilitățile necesare pentru a trata cu seriozitate și profesionalism toate cazurile mele. Misiunea mea este să obțin rezultate pozitive pentru clienții mei și să-i ajut să-și atingă obiectivele juridice.</p>
         </section>
         <section id="intro" className="bg-light mt-5">
           <div className="container">
@@ -31,14 +33,19 @@ const Acasa = () => {
                 <img src="welcome-img.jpg" className="img-fluid" alt="two people signing documents" />
               </div>
               <div className="col col-md-6 order-md-1 mt-5 mt-md-0">
-                <h4 className='mb-4'>Te confrunți cu o problemă juridică?</h4>
+                <h4 className='mb-4'>Cine suntem?</h4>
                 <p>CABINET DE AVOCAT ALINA MARIN îți stă la dispoziție. Imi asum misiunea de a oferi clienților mei cele mai bune sfaturi juridice și de a lupta pentru drepturile lor legale. Am cunoștințe solide de drept, o vastă experiență și abilitățile necesare pentru a trata cu seriozitate și profesionalism toate cazurile mele. Misiunea mea este să obțin rezultate pozitive pentru clienții mei și să-i ajut să-și atingă obiectivele juridice.</p>
                 <div className="btn-group">
                   <button type="button" className="btn btn-outline-secondary">SUNA ACUM</button>
                   <button type="button" className="btn btn-outline-secondary">LISTA SERVICII</button>
                 </div>
               </div>
+
             </div>
+            <section>
+              <h4 className='mb-4'>Ce ne recomanda</h4>
+              <p>CABINET DE AVOCAT ALINA MARIN îți stă la dispoziție. Imi asum misiunea de a oferi clienților mei cele mai bune sfaturi juridice și de a lupta pentru drepturile lor legale. Am cunoștințe solide de drept, o vastă experiență și abilitățile necesare pentru a trata cu seriozitate și profesionalism toate cazurile mele. Misiunea mea este să obțin rezultate pozitive pentru clienții mei și să-i ajut să-și atingă obiectivele juridice.</p>
+            </section>
           </div>
         </section>
 
