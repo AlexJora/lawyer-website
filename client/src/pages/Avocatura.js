3614492ceab9f5edc5a70ef5ec3e7f586262c5a5
AlexJora <jora.catalinaa@gmail.com>
2023-04-28 01:24:11 +0200

    icon and border-avocatura page

--- client/src/pages/Avocatura.js
+++ client/src/pages/Avocatura.js
@@ -9,8 +9,8 @@ const Avocatura = () => {
       <div>
         <Navbar />
         <section className='avocatura'>
-          <img src="Slice 1.png" className="img-fluid" alt="books" />
-          {/* services */}
+          {/* <img src="Slice 1.png" className="img-fluid" alt="books" /> */}
+
 
 
           <div className=" pt-4 text-center">
@@ -26,8 +26,8 @@ const Avocatura = () => {
                   <div class="overlay"></div>
                 </div>
                 <div className="card-body text-center py-4">
-                  <FontAwesomeIcon icon={faScaleBalanced} size='2x' className='pb-3' />
-                  <h6 className="card-title">DREPT CIVIL</h6>
+                  <FontAwesomeIcon icon={faScaleBalanced} size='2x' className='law-icon pb-3' />
+                  <h6 className="card-title pb-2">DREPT CIVIL</h6>
 
                   <p className="card-text mx-1">Servicii de consultanță juridică, asistare și
                     reprezentare în fața instanțelor de judecată, redactare cereri de chemare
@@ -36,19 +36,14 @@ const Avocatura = () => {
 
               </div>
             </div>
-
-
-
-
-
             <div className="col-lg-4 col-md-6 col-sm-12">
               <div className="card h-100">
                 <div class="criminal card-img-top" >
                   <div class="overlay"></div>
                 </div>
                 <div className="card-body text-center py-4">
-                  <FontAwesomeIcon icon={faGavel} size='2x' className='pb-3' />
-                  <h6 className="card-title">DREPT PENAL</h6>
+                  <FontAwesomeIcon icon={faGavel} size='2x' className='law-icon pb-3' />
+                  <h6 className="card-title pb-2">DREPT PENAL</h6>
                   <p className="card-text mx-1">Asistență și reprezentare juridică în fața organelor de urmărire
                     penală și în fața instanțelor de judecată, redactare cereri și plângeri împotriva măsurilor și
                     actelor de urmărire penală, redactare cerere de reabilitare.</p>
@@ -61,8 +56,8 @@ const Avocatura = () => {
                   <div class="overlay"></div>
                 </div>
                 <div className="card-body text-center py-4">
-                  <FontAwesomeIcon icon={faTruck} size='2x' className='pb-3' />
-                  <h6 className="card-title">EXECUTARE SILITA</h6>
+                  <FontAwesomeIcon icon={faTruck} size='2x' className='law-icon pb-3' />
+                  <h6 className="card-title pb-2">EXECUTARE SILITA</h6>
                   <p className="card-text mx-1">În materia executării silite, serviciile oferite sunt indiferent de calitatea pe care o aveți în această procedură – debitor sau creditor. Pasivitatea poate avea consecințe deosebit de grave în această materie, atât pentru creditor, cât și pentru debitor.</p>
                 </div>
               </div>
@@ -74,8 +69,8 @@ const Avocatura = () => {
                   <div class="overlay"></div>
                 </div>
                 <div className='card-body text-center py-4'>
-                  <FontAwesomeIcon icon={faPeopleLine} size='2x' className='pb-3' />
-                  <h6 className="card-title">DREPTUL FAMILIEI - DIVORT SI PARTAJ</h6>
+                  <FontAwesomeIcon icon={faPeopleLine} size='2x' className='pb-3 law-icon' />
+                  <h6 className="card-title pb-2">DREPTUL FAMILIEI - DIVORT SI PARTAJ</h6>
                   <p className="card-text mx-1">Servicii de asistare și reprezentare juridică în procesele de divorț, partaj, exercitarea autorității părintești, pensie de întreținere, program de vizită minor.</p>
                 </div>
               </div>
@@ -86,8 +81,8 @@ const Avocatura = () => {
                   <div class="overlay"></div>
                 </div>
                 <div className="card-body text-center py-4">
-                  <FontAwesomeIcon icon={faBriefcase} size='2x' className='pb-3' />
-                  <h6 className="card-title">DREPTUL MUNCII</h6>
+                  <FontAwesomeIcon icon={faBriefcase} size='2x' className='law-icon pb-3' />
+                  <h6 className="card-title pb-2">DREPTUL MUNCII</h6>
                   <p className="card-text mx-1">Oferim servicii de avocatură profesionale, concretizate prin redactarea contestației împotriva deciziei de concediere, împotriva deciziei de sancționare disciplinară, obligarea angajatorului la plata drepturilor de natură salarială, elaborarea de opinii juridice în dreptul muncii.</p>
                 </div>
               </div>
@@ -98,8 +93,8 @@ const Avocatura = () => {
                   <div class="overlay"></div>
                 </div>
                 <div className="card-body text-center py-4">
-                  <FontAwesomeIcon icon={faScroll} size='2x' className='pb-3' />
-                  <h6 className="card-title">DREPTUL COMERCIAL SI SOCIETAR</h6>
+                  <FontAwesomeIcon icon={faScroll} size='2x' className='law-icon pb-3' />
+                  <h6 className="card-title pb-2">DREPTUL COMERCIAL SI SOCIETAR</h6>
                   <p className="card-text mx-1">Înființări societăți comerciale la Registrul Comerțului, schimbare sediu social, înființare/radiere punct de lucru, redactare acte constitutive, hotărâri ale adunării generale a asociațiilor.</p>
                 </div>
               </div>
@@ -107,7 +102,7 @@ const Avocatura = () => {
 
 
           </div>
-          <p className='text-light'>Alte domenii de activitate în care vă oferim consultanță, asistare și reprezentare în fața instanțelor de judecată sau a altor instituții ale statului:RECUPERARE CREANTE, PENSII SI ALTE DREPTURI DE ASIGURARI SOCIALE, ASOCIAȚII DE PROPRIETARI, PLÂNGERI CONTRAVENȚIONALE AMENZI, DESPĂGUBIRI ACCIDENTE AUTO.</p>
+          <p className='text-danger'>Alte domenii de activitate în care vă oferim consultanță, asistare și reprezentare în fața instanțelor de judecată sau a altor instituții ale statului:RECUPERARE CREANTE, PENSII SI ALTE DREPTURI DE ASIGURARI SOCIALE, ASOCIAȚII DE PROPRIETARI, PLÂNGERI CONTRAVENȚIONALE AMENZI, DESPĂGUBIRI ACCIDENTE AUTO.</p>
         </section >
         <Footer />
       </div >
