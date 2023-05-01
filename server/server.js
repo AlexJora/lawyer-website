b3f6f56e601b89e6651e7dd10f65389448dc4881
AlexJora <jora.catalinaa@gmail.com>
2023-05-01 20:20:36 +0200

    last updates

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
+
 const express = require('express');
 const cors = require('cors');
-
-
+const path = require('path');
 const app = express();
-
-
 app.use(cors({
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST']
 }));
 
 app.use(express.json());
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
 
 app.post('/contact', (req, res) => {
   const { name, email, phone, message } = req.body;
@@ -21,8 +23,5 @@ app.post('/contact', (req, res) => {
 
 const port = process.env.PORT || 3100;
 app.listen(port, () => console.log(`Listening on port ${port}.......`));
-
-
-
 
 
