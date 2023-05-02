214aeaa5bf7bb5f80f8e1edcd3212c4cd6d16853
AlexJora <jora.catalinaa@gmail.com>
2023-05-03 00:21:45 +0200

    update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
