ea4c6f135bed0c841df8286ebe3abb85515c4785
AlexJora <jora.catalinaa@gmail.com>
2023-05-04 14:06:56 +0200

    adding emailjs

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,62 +1,77 @@
-import React, { useState } from "react";
+
+import { useState } from "react";
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
   const [status, setStatus] = useState("Trimite mesaj");
+
+  const sendMail = () => {
+    const params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value,
+      message: document.getElementById("query").value,
+    };
+
+    const serviceID = "service_5a3ti46";
+    const templateID = "template_1kdj04j";
+    const userID = "W_5JcMYz9DSqwaApi";
+
+    emailjs.send(serviceID, templateID, params, userID)
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        setStatus("Trimite mesaj");
+        alert("Mesajul tău a fost trimis cu succes!");
+      }, (error) => {
+        console.log("FAILED...", error);
+        setStatus("Trimite mesaj");
+        alert("Ceva nu a mers bine. Încearcă din nou mai târziu.");
+      });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setStatus("Se trimite...");
-    // const { name, email, phone, query } = e.target.elements;
-    // let details = {
-    //   name: name.value,
-    //   email: email.value,
-    //   phone: phone.value,
-    //   message: query.value,
-    // };
-    // let response = await fetch("http://localhost:3100/contact", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json;charset=utf-8",
-    //   },
-    //   body: JSON.stringify(details),
-    // });
 
-    // let response = await fetch("http://localhost:5000/contact", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json;charset=utf-8",
-    //   },
-    //   body: JSON.stringify(details),
-    // });
-    // setStatus("Trimite mesaj");
-    // let result = await response.json();
-    // alert(result.status);
+    sendMail();
   };
+
   return (
-    <div className='form-wrap p-5 mb-5'>
-      <div className='form-overlay'></div>
-      <div className=" contact-form text-center">
+    <div className="form-wrap p-5 mb-5">
+      <div className="form-overlay"></div>
+      <div className="contact-form text-center">
         <p className="lead text-light">Solicită consultanță</p>
       </div>
       <div className="row justify-content-center ">
         <div className="col-lg-12">
           <form onSubmit={handleSubmit}>
-            <label htmlFor="name" className="form-label text-light">Nume Prenume:</label>
+            <label htmlFor="name" className="form-label text-light">
+              Nume Prenume:
+            </label>
             <input type="text" id="name" className="form-control" required />
-            <label htmlFor="email" className="form-label text-light pt-2">Adresa de e-mail:</label>
+            <label htmlFor="email" className="form-label text-light pt-2">
+              Adresa de e-mail:
+            </label>
             <input type="email" id="email" className="form-control" required />
-            <label htmlFor="phone" className="form-label text-light pt-2">Număr de telefon:</label>
+            <label htmlFor="phone" className="form-label text-light pt-2">
+              Număr de telefon:
+            </label>
             <input type="tel" id="phone" className="form-control" required />
             <div>
-              <label htmlFor="query" className="form-label text-light pt-2">Mesajul dvs.:</label>
+              <label htmlFor="query" className="form-label text-light pt-2">
+                Mesajul dvs.:
+              </label>
               <textarea className="form-control" id="query" required></textarea>
             </div>
             <div className="my-4 text-center pt-2">
-              <button type="submit" className="btn btn-outline-dark ">{status}</button>
+              <button type="submit" className="btn btn-outline-dark">
+                {status}
+              </button>
             </div>
           </form>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
