29dd87ba1fe98788863318effaf9e82ab0c98fea
AlexJora <jora.catalinaa@gmail.com>
2023-05-08 00:30:19 +0200

    banner fixed again

--- client/src/components/TopBanner.js
+++ client/src/components/TopBanner.js
@@ -4,31 +4,39 @@ import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-
 
 const TopBanner = () => {
   return (
-
-    <div class='banner w-100 py-1'>
-      <div class='container'>
-        <div class='row'>
-          {/* First two columns */}
-          <div class='col-12 col-md-4 text-center'>
-            <FontAwesomeIcon icon={faLocationDot} className='pe-2' size='1x' />Bucuresti
-          </div>
-          <div class='col-12 col-md-4 text-center'>
-            <FontAwesomeIcon icon={faEnvelope} className='pe-2' size='1x' />av_alinamarin@yahoo.com
+    <div className='banner w-100 py-1'>
+      <div className='container'>
+        <div className='row justify-content-between align-items-center'>
+          {/* First column */}
+          <div className='col-5 col-md-4'>
+            <div className="d-flex flex-row align-items-center">
+              <FontAwesomeIcon icon={faLocationDot} className='pe-2' size='1x' />Bucuresti
+            </div>
           </div>
 
-          {/*Third column (hidden on small screens) */}
-          <div class='col-md-4 d-none d-md-block text-center'>
-            <FontAwesomeIcon icon={faPhone} className='phone-icon pe-2' size='1x' /> +40769 935 716
+          {/* Second column */}
+          <div className='col-7 col-md-4'>
+            <div className="d-flex flex-row align-items-center ">
+              <FontAwesomeIcon icon={faEnvelope} className='pe-2' size='1x' />av_alinamarin@yahoo.com
+            </div>
           </div>
 
-          {/* Third column (shown only on small screens)  */}
-          <div class='col-12 d-md-none text-center'>
-            <FontAwesomeIcon icon={faPhone} className='phone-icon pe-2' size='1x' /> +40769 935 716
+          {/* Third column (shown only on medium and large screens) */}
+          <div className='col-md-4 d-none d-md-block'>
+            <div className="d-flex flex-row align-items-center justify-content-end">
+              <FontAwesomeIcon icon={faPhone} className='phone-icon pe-2' size='1x' /> +40769 935 716
+            </div>
+          </div>
+
+          {/* Third column (shown only on small screens) */}
+          <div className='col-12 d-md-none text-center'>
+            <div className="d-flex flex-row align-items-center justify-content-center">
+              <FontAwesomeIcon icon={faPhone} className='phone-icon pe-2' size='1x' /> +40769 935 716
+            </div>
           </div>
         </div>
       </div>
     </div>
-
 
 
 
