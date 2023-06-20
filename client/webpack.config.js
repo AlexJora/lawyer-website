8d00eeb655b602af068493b1ac85d45d1238752c
AlexJora <jora.catalinaa@gmail.com>
2023-06-20 13:47:05 +0200

    babel, style loader, build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,6 +10,24 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
