5cdae19f277ae2c1487763d5b66c724e362fd2e3
AlexJora <jora.catalinaa@gmail.com>
2023-06-21 12:50:43 +0200

    code splitting in webpack

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,39 @@
 // import { Route, Routes } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Info from './pages/Info';
-import Avocatura from './pages/Avocatura';
-import Insolventa from './pages/Insolventa';
-import Contact from './pages/Contact';
-import Acasa from './pages/Acasa';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Info from './pages/Info';
+// import Avocatura from './pages/Avocatura';
+// import Insolventa from './pages/Insolventa';
+// import Contact from './pages/Contact';
+// import Acasa from './pages/Acasa';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer'
+const Info = React.lazy(() => import("./pages/Info"));
+const Avocatura = React.lazy(() => import("./pages/Avocatura"));
+const Insolventa = React.lazy(() => import("./pages/Insolventa"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const Acasa = React.lazy(() => import("./pages/Acasa"));
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Footer = React.lazy(() => import("./components/Footer"));
+
 function App() {
   return (
     <div className="App">
       <>
         <BrowserRouter>
-          <Navbar />
-          <Routes>
-
-            <Route path="/" element={<Acasa />} />
-            <Route path="/informatii-utile" element={<Info />} />
-            <Route path="/avocatura" element={<Avocatura />} />
-            <Route path="/insolventa" element={<Insolventa />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-
-          <Footer />
+          <React.Suspense fallback={<p>Loading...</p>}>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Acasa />} />
+              <Route path="/informatii-utile" element={<Info />} />
+              <Route path="/avocatura" element={<Avocatura />} />
+              <Route path="/insolventa" element={<Insolventa />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+          </React.Suspense>
         </BrowserRouter>
       </>
-
     </div>
   );
 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,8 +10,9 @@ module.exports = {
     main: "./src/index.js",
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
@@ -44,29 +45,12 @@ module.exports = {
         blockJSRequests: false,
       },
     }),
-    // new CriticalCssPlugin({
-    //   base: path.resolve(__dirname, "dist"),
-    //   src: "index.html",
-    //   // dest: "index.html",
-    //   inline: true,
-    //   // minify: true,
-    //   extract: true,
-    //   dimensions: [{ width: 1920, height: 1080 }],
-    // }),
   ],
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
     splitChunks: {
       chunks: "all",
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          chunks: "all",
-        },
-      },
     },
   },
 };
